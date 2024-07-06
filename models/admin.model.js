ade68ce452b528b156963ee656136122a132079d
Garvit <rw3.garvit.rd@gmail.com>
2024-07-06 12:17:16 +0530

    validation

--- models/admin.model.js
+++ models/admin.model.js
@@ -3,13 +3,9 @@ let mongoose = require("mongoose");
 let adminSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
-    trim: true,
   },
   password: {
     type: String,
-    required: true,
-    trim: true,
   },
 });
 
